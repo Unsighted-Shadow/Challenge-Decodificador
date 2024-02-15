58e55801ebdbb1f8eba8878b328540e7da8bd813
Unsighted-Shadow <carolsoeiro@gmail.com>
2024-02-15 08:36:19 -0300

    The encrypter

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 function encrypt() {
     var message = document.getElementById('message').value;
-    var encryptedMessage = message.replace(/e/g, 'enter');
-                                .replace(/i/g, 'imes');
-                                .replace(/a/g, 'ai');
-                                .replace(/o/g, 'ober');
+    var encryptedMessage = message.replace(/e/g, 'enter')
+                                .replace(/i/g, 'imes')
+                                .replace(/a/g, 'ai')
+                                .replace(/o/g, 'ober')
                                 .replace(/u/g, 'ufat');
     document.getElementById('message').value = encryptedMessage;
 }
 
 function decrypt() {
     var message = document.getElementById('message').value;
-    var decryptedMessage = message.replace(/enter/g, 'e');
-                                    .replace(/imes/g, 'i');
-                                    .replace(/ai/g, 'a');
-                                    .replace(/ober/g, 'o');
+    var decryptedMessage = message.replace(/enter/g, 'e')
+                                    .replace(/imes/g, 'i')
+                                    .replace(/ai/g, 'a')
+                                    .replace(/ober/g, 'o')
                                     .replace(/ufat/g, 'u');
     document.getElementById('message').value = decryptedMessage;
 }
